2acb1c5c1e63cbcbe31e1e8e07fbe75c93c26288
Maximilian Betz <Maximilian.Betz@awi.de>
2021-11-18 19:55:29 +0100

    CAN2 rx tx working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -356,6 +356,83 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+
+void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_TxMailbox1CompleteCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_TxMailbox2CompleteCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_TxMailbox0AbortCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_TxMailbox1AbortCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_TxMailbox2AbortCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+
+
+CAN_TxHeaderTypeDef rxfifo0_header;
+uint8_t rxfifo0_rxData[8];
+uint32_t rxfifo0 = 0;
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+  rxfifo0++;
+  HAL_CAN_GetRxMessage(&hcan2, 0, &rxfifo0_header, &rxfifo0_rxData);
+}
+
+void HAL_CAN_RxFifo0FullCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+CAN_TxHeaderTypeDef rxfifo1_header;
+uint8_t rxfifo1_rxData[8];
+uint32_t rxfifo1 = 0;
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+  rxfifo1++;
+  HAL_CAN_GetRxMessage(&hcan2, 1, &rxfifo1_header, &rxfifo1_rxData);
+}
+
+void HAL_CAN_RxFifo1FullCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_SleepCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_WakeUpFromRxMsgCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+
+
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -370,6 +447,31 @@ void StartDefaultTask(void *argument)
   /* init code for USB_DEVICE */
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 5 */
+
+  /*Receive all messages on fifo 0, do not filter yet*/
+  CAN_FilterTypeDef sFilterConfig;
+  sFilterConfig.FilterIdHigh = 0xFFFF;
+  sFilterConfig.FilterIdLow = 0x0000;
+  sFilterConfig.FilterMaskIdHigh = 0xFFFF;
+  sFilterConfig.FilterMaskIdLow = 0x0000;
+  sFilterConfig.FilterFIFOAssignment = 0;
+  sFilterConfig.FilterBank = 0;
+  sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  sFilterConfig.FilterScale = CAN_FILTERSCALE_16BIT;
+  sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
+  sFilterConfig.SlaveStartFilterBank = 0;
+
+
+  if (HAL_CAN_ConfigFilter(&hcan2, &sFilterConfig)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  if (HAL_CAN_ActivateNotification(&hcan2, CAN_IT_TX_MAILBOX_EMPTY|CAN_IT_RX_FIFO0_MSG_PENDING)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
   if (HAL_CAN_Start(&hcan2) != HAL_OK)
   {
     Error_Handler();
